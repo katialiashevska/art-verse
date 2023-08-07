61ffc8e027f7c6ccb64c0b9a31bb8c337c9d22bf
Katia <kate.liashevska@gmail.com>
2023-08-07 10:29:46 +0200

    tooltip optimisation for deploy

--- src/components/EachArtwork.jsx
+++ src/components/EachArtwork.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 
 function EachArtwork({ artwork }) {
     const [eachArtwork, setEachArtwork] = useState(null)
+    let tooltip = document.querySelectorAll(".tooltip")
 
     useEffect(() => {
         axios
@@ -11,6 +12,15 @@ function EachArtwork({ artwork }) {
             .catch(error => console.error(error.message))
     }, [artwork.api_link])
 
+    const tooltipMove = e => {
+        for (let i = tooltip.length; i--; ) {
+            tooltip[i].style.left = e.pageX + "px"
+            tooltip[i].style.top = e.pageY + "px"
+        }
+    }
+
+    document.addEventListener("mousemove", tooltipMove, false)
+
     if (!eachArtwork) {
         return <p>Loading artwork details...</p>
     }
@@ -18,8 +28,8 @@ function EachArtwork({ artwork }) {
     return (
         <article className="each-artwork">
             <div className="tooltip">
-                <p>{eachArtwork.title}</p>
-                <p>{eachArtwork.artist_title}</p>
+                <p className="artwork-title">{eachArtwork.title}</p>
+                <p className="artwork-artist">{eachArtwork.artist_title}, {eachArtwork.date_display}</p>
             </div>
             <img
                 className="artwork-img"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,6 @@ import EachArtwork from "../components/EachArtwork"
 
 function HomePage() {
     const [artworks, setArtworks] = useState(null)
-    let tooltip = document.querySelectorAll(".tooltip")
 
     useEffect(() => {
         axios
@@ -13,15 +12,6 @@ function HomePage() {
             .then(response => setArtworks(response.data.data))
             .catch(error => console.error(error.message))
     }, [])
-
-    const tooltipMove = e => {
-        for (let i = tooltip.length; i--; ) {
-            tooltip[i].style.left = e.pageX + "px"
-            tooltip[i].style.top = e.pageY + "px"
-        }
-    }
-
-    document.addEventListener("mousemove", tooltipMove, false)
 
     if (!artworks) {
         return <p>Loading artworks...</p>
