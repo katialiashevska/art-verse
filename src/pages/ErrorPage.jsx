7a50fc544e106eb8b1dac933ee14ff611de7d2da
Katia <kate.liashevska@gmail.com>
2023-08-30 19:18:40 +0200

    complete the comment feature & error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,19 @@
-import React from "react"
 import { Link } from "react-router-dom"
 import "../styles/error-page.css"
+import logo from "../assets/logo.svg"
+import error from "../assets/error.svg"
 
 function ErrorPage() {
     return (
         <div id="error-page">
+            <Link className="navbar-bg" to="/">
+                <img id="logo" src={logo} alt="ArtVerse logo" />
+            </Link>
             <h1 id="error-title">Oops, missing artworks!</h1>
             <Link id="error-back-button" to="/">
                 Back to homepage
             </Link>
+            <img id="error-image" src={error} alt="Error image" />
         </div>
     )
 }
